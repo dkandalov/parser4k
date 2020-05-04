494f169d9cfdfc6da2e6dee877ae9815d6c3b9f8
Dmitry Kandalov <dmitry.kandalov@gmail.com>
2020-05-04 18:04:49 +0100

    parser with left recursion, missing tests and other changes

--- src/test/kotlin/parser4k/core.kt
+++ src/test/kotlin/parser4k/core.kt
@@ -1,10 +1,12 @@
 package parser4k
 
-import parser4k.PlusMinusLanguageTests.Expression.*
-import parser4k.PlusMinusLanguageTests.OperatorType.Minus
-import parser4k.PlusMinusLanguageTests.OperatorType.Plus
+import parser4k.CommonParsers.token
+import parser4k.Expression.MinusExpression
+import parser4k.Expression.NumberLiteral
+import parser4k.Expression.PlusExpression
 import kotlin.test.Test
 import kotlin.test.assertEquals
+import kotlin.test.fail
 
 /*
  * Goals:
@@ -26,18 +28,18 @@ data class Input(val s: String, val offset: Int = 0) {
 
 data class Output<out T>(val payload: T, val input: Input)
 
-interface Parser<T> {
+interface Parser<out T> {
     fun parse(input: Input): Output<T>?
 }
 
-fun str(s: String) = object: Parser<String> {
+fun str(s: String) = object : Parser<String> {
     override fun parse(input: Input): Output<String>? {
         val (token, nextInput) = input.read(s.length) ?: return null
         return if (token != s) null else Output(token, nextInput)
     }
 }
 
-fun regex(pattern: String) = object: Parser<String> {
+fun regex(pattern: String) = object : Parser<String> {
     override fun parse(input: Input): Output<String>? {
         val matchResult = pattern.toRegex().find(input.s, input.offset) ?: return null
         if (matchResult.range.first != input.offset) return null
@@ -45,7 +47,7 @@ fun regex(pattern: String) = object: Parser<String> {
     }
 }
 
-fun <T> repeat(parser: Parser<T>, atLeast: Int = 0) = object: Parser<List<T>> {
+fun <T> repeat(parser: Parser<T>, atLeast: Int = 0) = object : Parser<List<T>> {
     override fun parse(input: Input): Output<List<T>>? {
         val payload = ArrayList<T>()
         var nextInput = input
@@ -58,33 +60,35 @@ fun <T> repeat(parser: Parser<T>, atLeast: Int = 0) = object: Parser<List<T>> {
     }
 }
 
-fun <T> or(vararg parsers: Parser<T>) = object: Parser<T> {
+fun <T> or(vararg parsers: Parser<T>) = object : Parser<T> {
     override fun parse(input: Input): Output<T>? {
-        parsers.forEach {
-            val output = it.parse(input)
+        parsers.forEach { parser ->
+            val output = parser.parse(input)
             if (output != null) return output
         }
         return null
     }
 }
 
-fun <T1> inOrder(parser1: Parser<T1>) = object: Parser<List1<T1>> {
-    override fun parse(input: Input): Output<List1<T1>>? {
-        val (payload, nextInput) = parser1.parse(input) ?: return null
-        return Output(List1(payload), nextInput)
+fun <T1> inOrder(parser1: Parser<T1>) =
+    object : Parser<List1<T1>> {
+        override fun parse(input: Input): Output<List1<T1>>? {
+            val (payload, nextInput) = parser1.parse(input) ?: return null
+            return Output(List1(payload), nextInput)
+        }
     }
-}
 
-fun <T1, T2> inOrder(parser1: Parser<T1>, parser2: Parser<T2>) = object: Parser<List2<T1, T2>> {
-    override fun parse(input: Input): Output<List2<T1, T2>>? {
-        val (payload, nextInput) = inOrder(parser1).parse(input) ?: return null
-        val (lastPayload, lastInput) = parser2.parse(nextInput) ?: return null
-        return Output(payload + lastPayload, lastInput)
+fun <T1, T2> inOrder(parser1: Parser<T1>, parser2: Parser<T2>) =
+    object : Parser<List2<T1, T2>> {
+        override fun parse(input: Input): Output<List2<T1, T2>>? {
+            val (payload, nextInput) = inOrder(parser1).parse(input) ?: return null
+            val (lastPayload, lastInput) = parser2.parse(nextInput) ?: return null
+            return Output(payload + lastPayload, lastInput)
+        }
     }
-}
 
 fun <T1, T2, T3> inOrder(parser1: Parser<T1>, parser2: Parser<T2>, parser3: Parser<T3>) =
-    object: Parser<List3<T1, T2, T3>> {
+    object : Parser<List3<T1, T2, T3>> {
         override fun parse(input: Input): Output<List3<T1, T2, T3>>? {
             val (payload, nextInput) = inOrder(parser1, parser2).parse(input) ?: return null
             val (lastPayload, lastInput) = parser3.parse(nextInput) ?: return null
@@ -93,7 +97,7 @@ fun <T1, T2, T3> inOrder(parser1: Parser<T1>, parser2: Parser<T2>, parser3: Pars
     }
 
 fun <T1, T2, T3, T4> inOrder(parser1: Parser<T1>, parser2: Parser<T2>, parser3: Parser<T3>, parser4: Parser<T4>) =
-    object: Parser<List4<T1, T2, T3, T4>> {
+    object : Parser<List4<T1, T2, T3, T4>> {
         override fun parse(input: Input): Output<List4<T1, T2, T3, T4>>? {
             val (payload, nextInput) = inOrder(parser1, parser2, parser3).parse(input) ?: return null
             val (lastPayload, lastInput) = parser4.parse(nextInput) ?: return null
@@ -115,6 +119,28 @@ data class List3<T1, T2, T3>(val value1: T1, val value2: T2, val value3: T3) {
 
 data class List4<T1, T2, T3, T4>(val value1: T1, val value2: T2, val value3: T3, val value4: T4)
 
+fun <T, R> Parser<T>.map(f: (T) -> R) = object : Parser<R> {
+    override fun parse(input: Input): Output<R>? {
+        val (payload, nextInput) = this@map.parse(input) ?: return null
+        return Output(f(payload), nextInput)
+    }
+}
+
+fun <T> ref(f: () -> Parser<T>) = object : Parser<T> {
+    override fun parse(input: Input) = f().parse(input)
+}
+
+fun <T> leftRef(f: () -> Parser<T>) = object : Parser<T> {
+    val offsets: HashSet<Int> = HashSet()
+
+    override fun parse(input: Input): Output<T>? {
+        if (!offsets.add(input.offset)) return null // Prevent stack overflow on left recursion
+        val output = f().parse(input)
+        offsets.remove(input.offset)
+        return output
+    }
+}
+
 
 class StrParserTests {
     @Test fun `it works`() {
@@ -127,11 +153,11 @@ class StrParserTests {
         str("foo").parse(Input("bar")) shouldEqual null
         str("foo").parse(Input("fo0")) shouldEqual null
 
-        // matches
+        // match
         str("foo").parse(Input("foo")) shouldEqual Output("foo", Input("foo", offset = 3))
         str("foo").parse(Input("foo__")) shouldEqual Output("foo", Input("foo__", offset = 3))
         str("foo").parse(Input("_foo_", offset = 1)) shouldEqual Output("foo", Input("_foo_", offset = 4))
-        str("foo").parse(Input("__foo", offset = 2), ) shouldEqual Output("foo", Input("__foo", offset = 5))
+        str("foo").parse(Input("__foo", offset = 2),) shouldEqual Output("foo", Input("__foo", offset = 5))
     }
 }
 
@@ -139,6 +165,7 @@ class RegexParserTests {
     @Test fun `it works`() {
         // not enough input
         regex("a").parse(Input("")) shouldEqual null
+        regex("a+").parse(Input("")) shouldEqual null
         regex("a").parse(Input("foo", offset = 3)) shouldEqual null
 
         // input mismatch
@@ -146,22 +173,71 @@ class RegexParserTests {
         regex("a").parse(Input("ba")) shouldEqual null
         regex("[abc]").parse(Input("zzz")) shouldEqual null
 
-        // matches
+        // match
         regex("a").parse(Input("a")) shouldEqual Output("a", Input("a", offset = 1))
+        regex("a*").parse(Input("a")) shouldEqual Output("a", Input("a", offset = 1))
+        regex("a*").parse(Input("aaa")) shouldEqual Output("aaa", Input("aaa", offset = 3))
         regex("a").parse(Input("abc")) shouldEqual Output("a", Input("abc", offset = 1))
         regex("[abc]").parse(Input("abc", offset = 0)) shouldEqual Output("a", Input("abc", offset = 1))
         regex("[abc]").parse(Input("abc", offset = 1)) shouldEqual Output("b", Input("abc", offset = 2))
-        regex("[abc]").parse(Input("abc", offset = 2), ) shouldEqual Output("c", Input("abc", offset = 3))
+        regex("[abc]").parse(Input("abc", offset = 2),) shouldEqual Output("c", Input("abc", offset = 3))
     }
 }
 
 class RepeatParserTests {
     @Test fun `it works`() {
-        inOrder(str("a"), str("b")).parse(Input("")) shouldEqual null
-        inOrder(str("a"), str("b")).parse(Input("a")) shouldEqual null
+        // not enough input
+        repeat(str("a"), atLeast = 1).parse(Input("")) shouldEqual null
+        repeat(str("a"), atLeast = 1).parse(Input("a", offset = 1)) shouldEqual null
+        repeat(str("a"), atLeast = 2).parse(Input("a")) shouldEqual null
 
-        inOrder(str("a")).parse(Input("abc")) shouldEqual Output(List1("a"), Input("abc", offset = 1))
-        inOrder(str("a"), str("b")).parse(Input("abc")) shouldEqual Output(List2("a", "b"), Input("abc", offset = 2))
+        // input mismatch
+        repeat(str("a"), atLeast = 1).parse(Input("b")) shouldEqual null
+
+        // match
+        repeat(str("a")).parse(Input("")) shouldEqual Output(emptyList<String>(), Input(""))
+        repeat(str("a")).parse(Input("b")) shouldEqual Output(emptyList<String>(), Input("b"))
+        repeat(str("a")).parse(Input("aaa")) shouldEqual Output(listOf("a", "a", "a"), Input("aaa", offset = 3))
+    }
+}
+
+class OrParserTests {
+    @Test fun `it works`() {
+        val abParser = or(str("a"), str("b"))
+
+        // not enough input
+        abParser.parse(Input("")) shouldEqual null
+        abParser.parse(Input("a", offset = 1)) shouldEqual null
+        abParser.parse(Input("b", offset = 1)) shouldEqual null
+
+        // input mismatch
+        abParser.parse(Input("c")) shouldEqual null
+
+        // match
+        abParser.parse(Input("ab")) shouldEqual Output("a", Input("ab", offset = 1))
+        abParser.parse(Input("ba")) shouldEqual Output("b", Input("ba", offset = 1))
+    }
+}
+
+class InOrderParserTests {
+    @Test fun `it works`() {
+        val abParser = inOrder(str("a"), str("b"))
+
+        // not enough input
+        abParser.parse(Input("")) shouldEqual null
+        abParser.parse(Input("a")) shouldEqual null
+        abParser.parse(Input("ab", offset = 1)) shouldEqual null
+
+        // input mismatch
+        abParser.parse(Input("foo")) shouldEqual null
+        abParser.parse(Input("aa")) shouldEqual null
+
+        // match
+        abParser.parse(Input("ab")) shouldEqual Output(List2("a", "b"), Input("ab", offset = 2))
+        abParser.parse(Input("ab__")) shouldEqual Output(List2("a", "b"), Input("ab__", offset = 2))
+        abParser.parse(Input("_ab_", offset = 1)) shouldEqual Output(List2("a", "b"), Input("_ab_", offset = 3))
+        abParser.parse(Input("__ab", offset = 2),) shouldEqual Output(List2("a", "b"), Input("__ab", offset = 4))
+
         inOrder(str("a"), str("b"), str("c")).parse(Input("abc")) shouldEqual Output(
             List3("a", "b", "c"),
             Input("abc", offset = 3)
@@ -173,122 +249,124 @@ class RepeatParserTests {
     }
 }
 
-class InOrderParserTests {
-    @Test fun `it works`() {
-        // not enough input
-        str("foo").parse(Input("")) shouldEqual null
-        str("foo").parse(Input("f")) shouldEqual null
-        str("foo").parse(Input("foo", offset = 1)) shouldEqual null
-
-        // input mismatch
-        str("foo").parse(Input("bar")) shouldEqual null
-        str("foo").parse(Input("fo0")) shouldEqual null
-
-        // matches
-        str("foo").parse(Input("foo")) shouldEqual Output("foo", Input("foo", offset = 3))
-        str("foo").parse(Input("foo__")) shouldEqual Output("foo", Input("foo__", offset = 3))
-        str("foo").parse(Input("_foo_", offset = 1)) shouldEqual Output("foo", Input("_foo_", offset = 4))
-        str("foo").parse(Input("__foo", offset = 2), ) shouldEqual Output("foo", Input("__foo", offset = 5))
-    }
+sealed class Expression {
+    data class NumberLiteral(val value: Int) : Expression()
+    data class PlusExpression(val left: Expression, val right: Expression) : Expression()
+    data class MinusExpression(val left: Expression, val right: Expression) : Expression()
 }
 
+object CommonParsers {
+    val whitespaces: Parser<String> = regex("[ \\t\\r\\n]*")
 
-fun <T, R> Parser<T>.map(f: (T) -> R) = object: Parser<R> {
-    override fun parse(input: Input): Output<R>? {
-        val (payload, nextInput) = this@map.parse(input) ?: return null
-        return Output(f(payload), nextInput)
-    }
+    fun token(s: String): Parser<String> =
+        inOrder(whitespaces, str(s), whitespaces).map { (_, op, _) -> op }
 }
 
+class PlusMinusParserTests {
+    private val numberTerm = regex("\\d+").map { NumberLiteral(it.toInt()) }
 
-class PlusMinusLanguageTests {
-    private val whitespaces = repeat(str(" "))
-    private val numberTerm: Parser<NumberLiteral> = regex("\\d+").map { NumberLiteral(it.toInt()) }
-    private val plusTerm: Parser<OperatorType> = inOrder(whitespaces, str("+"), whitespaces).map { Plus }
-    private val minusTerm: Parser<OperatorType> = inOrder(whitespaces, str("-"), whitespaces).map { Minus }
-
-    private val langExpr: Parser<Expression> = inOrder(numberTerm, repeat(inOrder(or(plusTerm, minusTerm), numberTerm)))
-        .map { (number, rest) ->
-            rest.fold(number as Expression) { expression, (operator, number) ->
+    private val expression = inOrder(numberTerm, repeat(inOrder(or(token("+"), token("-")), numberTerm)))
+        .map { (first, rest) ->
+            rest.fold(first as Expression) { left, (operator, right) ->
                 when (operator) {
-                    Plus  -> PlusExpression(expression, number)
-                    Minus -> MinusExpression(expression, number)
+                    "+"  -> PlusExpression(left, right)
+                    "-"  -> MinusExpression(left, right)
+                    else -> error("")
                 }
             }
         }
 
-    private enum class OperatorType { Plus, Minus }
-
-    private sealed class Expression {
-        data class NumberLiteral(val value: Int): Expression()
-        data class PlusExpression(val left: Expression, val right: Expression): Expression()
-        data class MinusExpression(val left: Expression, val right: Expression): Expression()
-    }
-
-    private fun Expression.eval(): Int =
-        when (this) {
-            is NumberLiteral   -> this.value
-            is MinusExpression -> left.eval() - right.eval()
-            is PlusExpression  -> left.eval() + right.eval()
-        }
-
-    @Test fun `123`() {
-        val expression = langExpr.parse(Input("123"))?.payload!!
-        expression shouldEqual NumberLiteral(123)
-        expression.eval() shouldEqual 123
-    }
-
-    @Test fun `1 + 2`() {
-        val expression = langExpr.parse(Input("1 + 2"))?.payload!!
-        expression shouldEqual PlusExpression(
-            NumberLiteral(1),
-            NumberLiteral(2)
-        )
-        expression.eval() shouldEqual 3
-    }
-
-    @Test fun `1 + 2 + 3`() {
-        val expression = langExpr.parse(Input("1 + 2 + 3"))?.payload!!
-        expression shouldEqual PlusExpression(
-            PlusExpression(
-                NumberLiteral(1),
-                NumberLiteral(2)
-            ),
+    @Test fun `valid input`() {
+        "123" shouldParseTo NumberLiteral(123)
+        "1 + 2" shouldParseTo PlusExpression(NumberLiteral(1), NumberLiteral(2))
+        "1 + 2 + 3" shouldParseTo PlusExpression(
+            PlusExpression(NumberLiteral(1), NumberLiteral(2)),
             NumberLiteral(3)
         )
-        expression.eval() shouldEqual 6
-    }
-
-    @Test fun `1 + 2 - 3`() {
-        val expression = langExpr.parse(Input("1 + 2 - 3"))?.payload!!
-        expression shouldEqual MinusExpression(
-            PlusExpression(
-                NumberLiteral(1),
-                NumberLiteral(2)
-            ),
+        "1 + 2 - 3" shouldParseTo MinusExpression(
+            PlusExpression(NumberLiteral(1), NumberLiteral(2)),
             NumberLiteral(3)
         )
-        expression.eval() shouldEqual 0
     }
 
     @Test fun `invalid input`() {
-        langExpr.parse(Input("abc")) shouldEqual null
-        langExpr.parse(Input("+123")) shouldEqual null
-        langExpr.parse(Input("-123")) shouldEqual null
+        expression.parse(Input("abc")) shouldEqual null
+        expression.parse(Input("+123")) shouldEqual null
+        expression.parse(Input("-123")) shouldEqual null
     }
 
     @Test fun `partial input`() {
-        langExpr.parse(Input("1 + 2 +")) shouldEqual Output(
+        expression.parse(Input("1 + 2 +")) shouldEqual Output(
             payload = PlusExpression(NumberLiteral(1), NumberLiteral(2)),
             input = Input(s = "1 + 2 +", offset = 5)
         )
-        langExpr.parse(Input("1 ++ 2")) shouldEqual Output(
+        expression.parse(Input("1 ++ 2")) shouldEqual Output(
             payload = NumberLiteral(1),
             input = Input(s = "1 ++ 2", offset = 1)
         )
     }
+
+    private infix fun String.shouldParseTo(expected: Expression) =
+        assertEquals(expected, expression.parseAllInputOrFail(this))
 }
 
-private infix fun Any?.shouldEqual(expected: Any?) {
+class PlusMinusWithRecursionParserTests {
+    private val numberTerm = regex("\\d+").map { NumberLiteral(it.toInt()) }
+
+    private val plusExpression = inOrder(leftRef { expression }, token("+"), ref { expression })
+        .map { (left, _, right) -> PlusExpression(left, right) }
+
+    private val minusExpression = inOrder(leftRef { expression }, token("-"), ref { expression })
+        .map { (left, _, right) -> MinusExpression(left, right) }
+
+    private val expression: Parser<Expression> = or(plusExpression, minusExpression, numberTerm)
+
+    @Test fun `valid input`() {
+        "123" shouldParseTo NumberLiteral(123)
+        "1 + 2" shouldParseTo PlusExpression(NumberLiteral(1), NumberLiteral(2))
+        "1 + 2 + 3" shouldParseTo PlusExpression(
+            NumberLiteral(1),
+            PlusExpression(NumberLiteral(2), NumberLiteral(3))
+        )
+        "1 + 2 - 3" shouldParseTo PlusExpression(
+            NumberLiteral(1),
+            MinusExpression(NumberLiteral(2), NumberLiteral(3))
+        )
+    }
+
+    @Test fun `invalid input`() {
+        expression.parse(Input("abc")) shouldEqual null
+        expression.parse(Input("+123")) shouldEqual null
+        expression.parse(Input("-123")) shouldEqual null
+    }
+
+    @Test fun `partial input`() {
+        expression.parse(Input("1 + 2 +")) shouldEqual Output(
+            payload = PlusExpression(NumberLiteral(1), NumberLiteral(2)),
+            input = Input(s = "1 + 2 +", offset = 5)
+        )
+        expression.parse(Input("1 ++ 2")) shouldEqual Output(
+            payload = NumberLiteral(1),
+            input = Input(s = "1 ++ 2", offset = 1)
+        )
+    }
+
+    private infix fun String.shouldParseTo(expected: Expression) =
+        assertEquals(expected, expression.parseAllInputOrFail(this))
+}
+
+private infix fun Any?.shouldEqual(expected: Any?) =
     assertEquals(expected = expected, actual = this)
+
+private fun Parser<*>.parseAllInputOrFail(s: String): Any? {
+    val (payload, input) = parse(Input(s)) ?: fail("Couldn't parse '$s'")
+    if (input.offset < input.s.length) {
+        fail(
+            "Input was not fully consumed:\n" +
+                "$s\n" +
+                " ".repeat(input.offset) + "^\n" +
+                "payload = $payload"
+        )
+    }
+    return payload
 }
