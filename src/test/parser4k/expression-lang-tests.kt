1b9a91d8e93b6edf0352dc9195efdcfa1a472f9d
Dmitry Kandalov <dmitry.kandalov@gmail.com>
2020-05-17 13:37:45 +0100

    added unary and parens to ExpressionLang

--- src/test/parser4k/expression-lang-tests.kt
+++ src/test/parser4k/expression-lang-tests.kt
@@ -27,9 +27,11 @@ private object ExpressionLang {
         data class Minus(val left: Expr, val right: Expr) : Expr()
         data class Multiply(val left: Expr, val right: Expr) : Expr()
         data class Divide(val left: Expr, val right: Expr) : Expr()
+        data class UnaryMinus(val value: Expr) : Expr()
 
         data class And(val left: Expr, val right: Expr) : Expr()
         data class Or(val left: Expr, val right: Expr) : Expr()
+        data class Not(val value: Expr) : Expr()
 
         data class InArray(val left: Expr, val right: Expr) : Expr()
         data class NotInArray(val left: Expr, val right: Expr) : Expr()
@@ -39,6 +41,9 @@ private object ExpressionLang {
 
     private fun binaryExpr(tokenString: String, f: (Expr, Expr) -> Expr) =
         inOrder(ref { expr }, token(tokenString), ref { expr }).leftAssocAsBinary(f).with(cache)
+
+    private fun unaryExpr(tokenString: String, f: (Expr) -> Expr) =
+        inOrder(token(tokenString), ref { expr }).map { (_, it) -> f(it) }.with(cache)
 
     private val boolLiteral = oneOf(str("true"), str("false")).map { if (it == "true") True else False }
     private val intLiteral = CommonParsers.integer.map { IntLiteral(it.toInt()) }
@@ -56,12 +61,16 @@ private object ExpressionLang {
     private val multiply = binaryExpr("*", ::Multiply)
     private val minus = binaryExpr("-", ::Minus)
     private val plus = binaryExpr("+", ::Plus)
+    private val unaryMinus = unaryExpr("-", ::UnaryMinus)
 
     private val inArray = binaryExpr("in", ::InArray)
     private val notInArray = binaryExpr("not in", ::NotInArray)
 
     private val and = binaryExpr("and", ::And)
     private val or = binaryExpr("or", ::Or)
+    private val not = unaryExpr("not", ::Not)
+
+    private val paren = inOrder(token("("), ref { expr }, token(")")).map { (_, it, _) -> it }.with(cache)
 
     private val expr: Parser<Expr> = oneOfWithPrecedence(
         or,
@@ -70,6 +79,8 @@ private object ExpressionLang {
         oneOf(equal, notEqual, less, greater),
         oneOf(plus, minus),
         oneOf(multiply, divide),
+        oneOf(unaryMinus, not),
+        paren.nestedPrecedence(),
         oneOf(arrayLiteral, stringLiteral, intLiteral, boolLiteral)
     ).reset(cache)
 
@@ -94,14 +105,15 @@ private object ExpressionLang {
             is Minus         -> (left.eval() as Int) - (right.eval() as Int)
             is Multiply      -> (left.eval() as Int) * (right.eval() as Int)
             is Divide        -> (left.eval() as Int) / (right.eval() as Int)
+            is UnaryMinus    -> -(value.eval() as Int)
 
             is InArray       -> left.eval() in (right.eval() as List<*>)
             is NotInArray    -> left.eval() !in (right.eval() as List<*>)
 
             is And           -> (left.eval() as Boolean) && (right.eval() as Boolean)
             is Or            -> (left.eval() as Boolean) || (right.eval() as Boolean)
+            is Not           -> !(value.eval() as Boolean)
         }
-
 }
 
 class ExpressionLangTests {
@@ -148,11 +160,12 @@ class ExpressionLangTests {
         evaluate("1 not in [1, 2, 3]") shouldEqual false
     }
 
-    @Test fun `and-or expressions`() {
+    @Test fun `and, or, not expressions`() {
         evaluate("1==1 and 2==2") shouldEqual true
         evaluate("1==1 and 2==3") shouldEqual false
         evaluate("1==1 or 2==2") shouldEqual true
         evaluate("1==2 or 2==3") shouldEqual false
+        evaluate("not false") shouldEqual true
 
         parse("1 and 2 or 3") shouldEqual Or(And(IntLiteral(1), IntLiteral(2)), IntLiteral(3))
         parse("1 or 2 and 3") shouldEqual Or(IntLiteral(1), And(IntLiteral(2), IntLiteral(3)))
@@ -164,5 +177,17 @@ class ExpressionLangTests {
         evaluate("1 - 2 + 3") shouldEqual 2
         evaluate("1 + 2 * 3") shouldEqual 7
         evaluate("1 + 2 * 3 / 4") shouldEqual 2
+
+        evaluate("-1 + 2") shouldEqual 1
+        evaluate("-1 + -2") shouldEqual -3
+        evaluate("-1 - -2") shouldEqual 1
+    }
+
+    @Test fun `paren expressions`() {
+        evaluate("(123)") shouldEqual 123
+        evaluate("((123))") shouldEqual 123
+        evaluate("((1 + 2) * 3 + 4)") shouldEqual 13
+
+        parse("not (1 or 2)") shouldEqual Not(Or(IntLiteral(1), IntLiteral(2)))
     }
 }
