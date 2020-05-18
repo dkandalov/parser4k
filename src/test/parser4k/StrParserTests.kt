c6191b4ee0e37361e5533a4221d91ba41124e529
Dmitry Kandalov <dmitry.kandalov@gmail.com>
2020-05-18 18:14:34 +0100

    refactoring

--- src/test/parser4k/StrParserTests.kt
+++ src/test/parser4k/StrParserTests.kt
@@ -4,19 +4,21 @@ import kotlin.test.Test
 
 class StrParserTests {
     @Test fun `it works`() {
+        val parser = str("foo")
+
         // not enough input
-        str("foo").parse(Input("")) shouldEqual null
-        str("foo").parse(Input("f")) shouldEqual null
-        str("foo").parse(Input("foo", offset = 1)) shouldEqual null
+        parser.parse(Input("")) shouldEqual null
+        parser.parse(Input("f")) shouldEqual null
+        parser.parse(Input("foo", offset = 1)) shouldEqual null
 
         // input mismatch
-        str("foo").parse(Input("bar")) shouldEqual null
-        str("foo").parse(Input("fo0")) shouldEqual null
+        parser.parse(Input("bar")) shouldEqual null
+        parser.parse(Input("fo0")) shouldEqual null
 
         // match
-        str("foo").parse(Input("foo")) shouldEqual Output("foo", Input("foo", offset = 3))
-        str("foo").parse(Input("foo__")) shouldEqual Output("foo", Input("foo__", offset = 3))
-        str("foo").parse(Input("_foo_", offset = 1)) shouldEqual Output("foo", Input("_foo_", offset = 4))
-        str("foo").parse(Input("__foo", offset = 2)) shouldEqual Output("foo", Input("__foo", offset = 5))
+        parser.parse(Input("foo")) shouldEqual Output("foo", Input("foo", offset = 3))
+        parser.parse(Input("foo__")) shouldEqual Output("foo", Input("foo__", offset = 3))
+        parser.parse(Input("_foo_", offset = 1)) shouldEqual Output("foo", Input("_foo_", offset = 4))
+        parser.parse(Input("__foo", offset = 2)) shouldEqual Output("foo", Input("__foo", offset = 5))
     }
 }
