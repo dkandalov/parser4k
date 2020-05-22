e737c4fa951960332c21a83e1d883a43c0f1646e
Dmitry Kandalov <dmitry.kandalov@gmail.com>
2020-05-22 11:49:08 +0100

    specify return type explicitly in generated code

--- src/test/parser4k/generate-in-order.kt
+++ src/test/parser4k/generate-in-order.kt
@@ -19,7 +19,7 @@ private class CodeGenerator(private val println: (String) -> Unit) {
     }
 
     private fun generateHeader() {
-        println("""@file:Suppress("UNCHECKED_CAST")""")
+        println("""@file:Suppress("UNCHECKED_CAST", "unused")""")
         println("")
         println("package parser4k")
         println("")
@@ -31,42 +31,46 @@ private class CodeGenerator(private val println: (String) -> Unit) {
 
     private fun generateSkipFirst() {
         // For example:
-        // fun <T2, T3> InOrder3<*, T2, T3>.skipFirst() = map { (_, it2, it3) -> List2(it2, it3) }
-        (2..8).forEach { n ->
+        // fun <T2, T3> InOrder3<*, T2, T3>.skipFirst(): Parser<List2<T2, T3>> = map { (_, it2, it3) -> List2(it2, it3) }
+
+        println("fun <T2> InOrder2<*, T2>.skipFirst(): Parser<T2> = map { (_, it2) -> it2 }") // Special case because returning List1 is pointless
+        (3..8).forEach { n ->
             val ts = (2..n).joinToString { "T$it" }
             val its = (2..n).joinToString { "it$it" }
-            println("fun <$ts> InOrder$n<*, $ts>.skipFirst() = map { (_, $its) -> List${n - 1}($its) }")
+            println("fun <$ts> InOrder$n<*, $ts>.skipFirst(): Parser<List${n - 1}<$ts>> = map { (_, $its) -> List${n - 1}($its) }")
         }
         println("")
     }
 
     private fun generateSkipLast() {
         // For example:
-        // fun <T1, T2> InOrder3<T1, T2, *>.skipLast() = map { (it1, it2, _) -> List2(it1, it2) }
-        (1..7).forEach { n ->
+        // fun <T1, T2> InOrder3<T1, T2, *>.skipLast(): Parser<List2<T1, T2>> = map { (it1, it2, _) -> List2(it1, it2) }
+
+        println("fun <T1> InOrder2<T1, *>.skipLast(): Parser<T1> = map { (it1, _) -> it1 }") // Special case because returning List1 is pointless
+        (2..7).forEach { n ->
             val ts = (1..n).joinToString { "T$it" }
             val its = (1..n).joinToString { "it$it" }
-            println("fun <$ts> InOrder${n + 1}<$ts, *>.skipLast() = map { ($its, _) -> List$n($its) }")
+            println("fun <$ts> InOrder${n + 1}<$ts, *>.skipLast(): Parser<List$n<$ts>> = map { ($its, _) -> List$n($its) }")
         }
         println("")
     }
 
     private fun generateSkipWrapper() {
         // For example:
-        // fun <T2, T3> InOrder4<*, T2, T3, *>.skipWrapper() = map { (_, it2, it3, _) -> List2(it2, it3) }
+        // fun <T2, T3> InOrder4<*, T2, T3, *>.skipWrapper(): Parser<List2<T2, T3>> = map { (_, it2, it3, _) -> List2(it2, it3) }
 
-        println("fun <T2> InOrder3<*, T2, *>.skipWrapper() = map { (_, it2, _) -> it2 }") // Special case because returning List1 is pointless
+        println("fun <T2> InOrder3<*, T2, *>.skipWrapper(): Parser<T2> = map { (_, it2, _) -> it2 }") // Special case because returning List1 is pointless
         (3..7).forEach { n ->
             val ts = (2..n).joinToString { "T$it" }
             val its = (2..n).joinToString { "it$it" }
-            println("fun <$ts> InOrder${n + 1}<*, $ts, *>.skipWrapper() = map { (_, $its, _) -> List${n - 1}($its) }")
+            println("fun <$ts> InOrder${n + 1}<*, $ts, *>.skipWrapper(): Parser<List${n - 1}<$ts>> = map { (_, $its, _) -> List${n - 1}($its) }")
         }
         println("")
     }
 
     private fun generateLeftAssoc() {
         // For example:
-        // fun <T1, T2, T3> InOrder3<T1, T2, T3>.leftAssoc(transform: (List3<T1, T2, T3>) -> T1) =
+        // fun <T1, T2, T3> InOrder3<T1, T2, T3>.leftAssoc(transform: (List3<T1, T2, T3>) -> T1): Parser<T1> =
         //    InOrder(listOf(parser1, parser2, parser3))
         //        .leftAssoc { (it1, it2, it3) -> transform(List3(it1 as T1, it2 as T2, it3 as T3)) } as Parser<T1>
 
@@ -76,7 +80,7 @@ private class CodeGenerator(private val println: (String) -> Unit) {
             val itsAsTs = (1..n).joinToString { "it$it as T$it" }
             val parsers = (1..n).joinToString { "parser$it" }
             println("""
-                fun <$ts> InOrder$n<$ts>.leftAssoc(transform: (List$n<$ts>) -> T1) =
+                fun <$ts> InOrder$n<$ts>.leftAssoc(transform: (List$n<$ts>) -> T1): Parser<T1> =
                     InOrder(listOf($parsers))
                         .leftAssoc { ($its) -> transform(List$n($itsAsTs)) } as Parser<T1>
             """.trimIndent())
