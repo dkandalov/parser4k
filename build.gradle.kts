236fd6a19633d11ed057af372733765ea064dffc
Dmitry Kandalov <dmitry.kandalov@gmail.com>
2020-05-24 22:47:20 +0100

    publishing to bintray

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,20 +1,21 @@
-import org.jetbrains.kotlin.gradle.plugin.*
-import org.jetbrains.kotlin.gradle.tasks.*
+import org.jetbrains.kotlin.gradle.plugin.KotlinSourceSet
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+import java.net.URI
 
 plugins {
     java
     kotlin("jvm") version "1.3.72"
+    `maven-publish`
 }
 group = "parser4k"
 version = "0.01"
 
 repositories {
     mavenCentral()
-    maven ("https://dl.bintray.com/kotlin/kotlin-eap")
 }
 
 dependencies {
-    implementation(kotlin("stdlib"))
+    compileOnly(kotlin("stdlib"))
     testImplementation(kotlin("test"))
     testImplementation(kotlin("test-junit"))
     testImplementation(kotlin("test-annotations-common"))
@@ -33,4 +34,25 @@ tasks.withType<KotlinCompile>().configureEach {
 tasks.withType<Test>().configureEach {
     // Larger stack for performance tests on large inputs
     jvmArgs("-Xss10M")
+}
+
+java {
+    withSourcesJar()
+}
+
+publishing {
+    publications {
+        create<MavenPublication>("parser4k") {
+            from(components["java"])
+        }
+    }
+    repositories {
+        maven {
+            url = URI("https://api.bintray.com/maven/dkandalov/maven/parser4k/;publish=1")
+            credentials {
+                username = System.getenv("BINTRAY_USER")
+                password = System.getenv("BINTRAY_API_KEY")
+            }
+        }
+    }
 }
