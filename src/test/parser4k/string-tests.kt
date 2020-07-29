3f973a110db85d024e18d489634884ef123f2d23
Dmitry Kandalov <dmitry.kandalov@gmail.com>
2020-07-29 14:56:46 +0100

    make test files match source code file names

--- src/test/parser4k/string-tests.kt
+++ src/test/parser4k/string-tests.kt
@@ -2,6 +2,27 @@ package parser4k
 
 import kotlin.test.Test
 
+class StringParserTests {
+    @Test fun `it works`() {
+        val parser = str("foo")
+
+        // not enough input
+        parser.parse(Input("")) shouldEqual null
+        parser.parse(Input("f")) shouldEqual null
+        parser.parse(Input("foo", offset = 1)) shouldEqual null
+
+        // input mismatch
+        parser.parse(Input("bar")) shouldEqual null
+        parser.parse(Input("fo0")) shouldEqual null
+
+        // match
+        parser.parse(Input("foo")) shouldEqual Output("foo", Input("foo", offset = 3))
+        parser.parse(Input("foo__")) shouldEqual Output("foo", Input("foo__", offset = 3))
+        parser.parse(Input("_foo_", offset = 1)) shouldEqual Output("foo", Input("_foo_", offset = 4))
+        parser.parse(Input("__foo", offset = 2)) shouldEqual Output("foo", Input("__foo", offset = 5))
+    }
+}
+
 class RegexParserTests {
     @Test fun `it works`() {
         // not enough input
